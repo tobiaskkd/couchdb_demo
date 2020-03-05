1f2c1734b5e4f292dbed8ce09f121af3f1ad577c
Tobias Karentius Kromann Dahl <tobi@html24.net>
2020-03-05 13:43:00 +0100

    did everything

---
+++ src/main/java/JavaCouchDB.java
@@ -0,0 +1,24 @@
+//package couchdb;
+import java.net.MalformedURLException;
+import org.ektorp.CouchDbConnector;
+import org.ektorp.CouchDbInstance;
+import org.ektorp.http.HttpClient;
+import org.ektorp.http.StdHttpClient;
+import org.ektorp.impl.StdCouchDbConnector;
+import org.ektorp.impl.StdCouchDbInstance;
+import org.ektorp.support.DesignDocument;
+public class JavaCouchDB {
+    public static void main(String[] args) throws MalformedURLException {
+//--------------- Creating Connection--------------------------//
+        HttpClient httpClient = new StdHttpClient.Builder()
+                .url("http://localhost:5984")
+                .build();
+        CouchDbInstance dbInstance = new StdCouchDbInstance(httpClient);
+//--------------- Creating database----------------------------//
+        CouchDbConnector db = new StdCouchDbConnector("couchdb_demo", dbInstance);
+        //db.createDatabaseIfNotExists();
+//--------------- Creating Document----------------------------//
+        DesignDocument dd = new DesignDocument("tis");
+        db.create(dd);
+    }
+}
